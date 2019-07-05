dd00505b5c6423d7781af8c74b71e8c7b06b81f9
Sallet <erika.sallet@inra.fr>
2019-07-05 18:18:49 +0200

    configure lint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,15 +1,16 @@
 module.exports = {
   root: true,
   env: {
-    node: true
+    node: true,
   },
   extends: ['airbnb', 'prettier', 'plugin:vue/essential', '@vue/airbnb'],
   plugins: ['prettier'],
   rules: {
+    // 'import/no-useless-path-segments': 'off',
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
   },
   parserOptions: {
-    parser: 'babel-eslint'
-  }
+    parser: 'babel-eslint',
+  },
 };
