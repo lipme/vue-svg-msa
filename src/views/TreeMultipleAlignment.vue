5df1b33fcbcf9f505f7fec7df164b53c94a92416
Sallet <erika.sallet@inra.fr>
2019-07-29 18:23:42 +0200

    display a svg-tree-alignment-graphic.

--- src/views/TreeMultipleAlignment.vue
+++ src/views/TreeMultipleAlignment.vue
@@ -8,7 +8,7 @@
     <section v-else>
       <div v-if="loading">Loading...</div>
       <div v-else>
-        <svg-alignment-graphic :seqs="seqs" :unsteady="true"></svg-alignment-graphic>
+        <svg-tree-alignment-graphic :track="track" :tree="tree"></svg-tree-alignment-graphic>
       </div>
     </section>
   </section>
@@ -16,12 +16,12 @@
 
 <script>
 import axios from 'axios';
-import SvgAlignmentGraphic from '@/components/svg/SvgAlignmentGraphic.vue';
+import SvgTreeAlignmentGraphic from '@/components/svg/SvgTreeAlignmentGraphic.vue';
 
 export default {
   name: 'TreeMultipleAlignment',
   components: {
-    SvgAlignmentGraphic
+    SvgTreeAlignmentGraphic
   },
   props: {
     width: { type: Number },
@@ -31,24 +31,21 @@ export default {
     return {
       errored: false,
       loading: true,
-      fileName: 'isc-20190725-37geno.pc80.ATLAS.fa.Chr01g0000001.json',
-      tree: {}
+      jsonFileName: 'isc-20190725-37geno.pc80.ATLAS.fa.Chr01g0000001.json',
+      trackFileName: 'exonTrack.json',
+      tree: {},
+      track: {}
     };
   },
   mounted() {
-    this.LoadTestFile();
+    this.LoadJsonTreeFile();
+    this.LoadJsonTrackFile();
   },
   created() {},
-  computed: {
-    seqs() {
-        return this.getSequencesByNodeNumberAndByDepth(1,3)
-      //return this.getSequencesByNodeNumber(5);
-    }
-  },
   methods: {
-    LoadTestFile() {
+    LoadJsonTreeFile() {
       axios
-        .get(this.fileName)
+        .get(this.jsonFileName)
         .then(resp => {
           this.tree = resp.data;
         })
@@ -59,69 +56,18 @@ export default {
           this.loading = false;
         });
     },
-    /* return the node object which has the number n */
-    getNode(n) {
-      if (this.loading === false) return this.getNodeInObject(this.tree, n);
-    },
-    /* search, in the object o, the node object which has the number n and return it */
-    getNodeInObject(o, n) {
-      if (o.node_number == n) {
-        return o;
-      } else {
-        if (o.nodes.length > 0) {
-          var aa = o.nodes.map(node => this.getNodeInObject(node, n)).filter(a => a != null);
-          if (aa.length > 0) {
-            return aa[0];
-          } else {
-            return null;
-          }
-        } else {
-          return null;
-        }
-      }
-    },
-    /* return an array composed of the sequences containing under the nodes numbered n */
-    getSequencesByNodeNumber(n) {
-      const node = this.getNode(n);
-      var seqs = [];
-      this.getSequencesByNode(node, seqs);
-      // add the consensus sequence 
-      seqs.push(this.buildConsensusSeq(node.consensus, node.node_number))
-      return seqs;
-    },
-    /* search the sequences containing under the nodes node, and add them in the array seqs */
-    getSequencesByNode(node, seqs) {
-      if (node.leaves.length > 0) {
-        node.leaves.forEach(l => seqs.push(l));
-      }
-      node.nodes.forEach(n => this.getSequencesByNode(n, seqs));
-    },
-
-    getSequencesByNodeNumberAndByDepth(n, depth){
-      var seqs = [];
-      const node = this.getNode(n);
-      this.getSequencesByDepth(node, depth, seqs)
-      return seqs
-    },
-    getSequencesByDepth(node, depth, seqs)
-    {
-        if (depth == 0)
-        {
-            seqs.push(this.buildConsensusSeq(node.consensus, node.node_number))
-        }
-        else
-        {   
-            node.leaves.forEach(l => seqs.push(l));
-            node.nodes.forEach(n => this.getSequencesByDepth(n, depth-1,seqs));
-        }
-    },
-
-    buildConsensusSeq(sequence, nb) {
-        return {"leave_number": nb, 
-                    "name": "CONSENSUS",
-                    "seq": sequence,
-                    "isConsensus": "true"}
-    },
+    LoadJsonTrackFile() {
+      axios
+      .get(this.trackFileName)
+        .then(resp => {
+          this.track = resp.data;
+        })
+        .catch(error => {
+          console.log("Error during track loading of " + this.trackFileName)
+        })
+        .finally(() => {
+        });
+    }
   }
 };
 </script>
