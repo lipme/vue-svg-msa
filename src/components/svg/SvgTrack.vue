7a4578989ae5d5473bddeb7a9a69e2405d35b55c
Sallet <erika.sallet@inra.fr>
2019-07-29 18:13:40 +0200

    add components to add a metadata track

---
+++ src/components/svg/SvgTrack.vue
@@ -0,0 +1,55 @@
+<template>
+  <g>
+    <svg-track-label :y="y" :maxWidth="fctScaleX(0) + 1" :label="track.trackLabel" :textFontSize="textFontSize"></svg-track-label>
+    <svg-track-content :y="y" :track="track" :aX="fctScaleX" :height="height"></svg-track-content>
+  </g>
+</template>
+
+<script>
+import SvgTrackLabel from '@/components/svg/SvgTrackLabel.vue'
+import SvgTrackContent from '@/components/svg/SvgTrackContent.vue'
+export default {
+    components:{
+    SvgTrackLabel,
+    SvgTrackContent
+    },
+  props: {
+    track: {type: Object},
+    y: { type: Number },
+    fctScaleX: { type: Function },
+    length: {type: Number},
+    textFontSize: {type: Number, default: 10}
+  },
+  data() {
+    return {
+    step: 10,
+    height: 10,
+    }
+  },
+  methods: {
+    getScaleWidth(i) {
+      return i === 1 || (i % this.step) === 0 ? '2px' : '1px';
+    },
+    getScaleHeight(i) {
+      return i === 1 || (i % this.step) === 0 ? '8px' : '7px';
+    },
+  },
+  computed:
+  {
+    getGlobalWidth()
+    {
+      var l = this.fctScaleX(this.length-1) - this.fctScaleX(0) + 5
+      return l
+    }
+  }
+};
+</script>
+
+<style scoped>
+.numberText {
+  fill: black;
+  text-anchor: middle;
+  font-family: 'monospace';
+}
+
+</style>
---
+++ src/components/svg/SvgTrackLabel.vue
@@ -0,0 +1,35 @@
+<template>
+  <text :x="1" :y="y" :font-size="textFontSize" fill="black" font-family="monospace">
+    <title>{{ label }}</title>
+    {{ displayedLabel }}
+  </text>
+</template>
+
+<script>
+export default {
+  props: {
+    label: { type: String },
+    y: { type: Number },
+    maxWidth: { type: Number },
+    textFontSize: { type: Number, default: 10 },
+  },
+  computed: {
+    displayLabelLength() {
+      return this.maxWidth / 10;
+    },
+    displayedLabel() {
+      if (this.label.length >= this.displayLabelLength) {
+        let title = '';
+        const splitName = this.label.split('');
+        splitName.forEach(x => {
+          if (title.length <= this.displayLabelLength) title = title.concat(String(x));
+        });
+        title = title.concat('...');
+        return title;
+      } else {
+        return this.label;
+      }
+    }
+  }
+};
+</script>
