0cb68ed3f436dfc65f86dbbb2c9d5a9b60b750d4
sallet <erika.sallet@inrae.fr>
2020-03-13 11:31:33 +0100

    smiplification

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,39 @@
 <template>
-  <v-app>
-    <v-content>
-      <router-view></router-view>
-    </v-content> 
-  </v-app>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+     <svg-seq-alignment
+      :start="rangeSlider[0]"
+      :end="rangeSlider[1]"
+      :seqs="seqs"
+      :tracks="tracks"
+      :selectedseqs="selectedSeqs"
+      :coloring="coloring"
+      v-on="$listeners"
+    ></svg-seq-alignment>
+  </div>
 </template>
 
 <script>
-import MultipleAlignment from './views/MultipleAlignment.vue';
+import HelloWorld from './components/HelloWorld.vue'
+import SvgSeqAlignment from './components/alignment/svg/SvgSeqAlignment.vue'
 
 export default {
   name: 'App',
   components: {
-    MultipleAlignment,
-  },
-};
+    HelloWorld,
+    SvgSeqAlignment,
+  }
+}
 </script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
