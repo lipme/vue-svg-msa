4f8df7f3a06b78b072f4786553d7024b365d399f
sallet <erika.sallet@inrae.fr>
2020-03-31 09:14:35 +0200

    remove listener

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,6 @@
       :tracks="tracks"
       :coloring="coloring"
       unsteady="true"
-      v-on="$listeners"
     ></svg-msa>
   </div>
 </template>
@@ -89,14 +88,3 @@ export default {
   }
 };
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
