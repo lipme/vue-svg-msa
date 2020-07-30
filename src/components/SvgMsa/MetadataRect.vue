20088b032881b246e2a65d32896d643b9a294f11
sallet <erika.sallet@inrae.fr>
2020-07-30 16:36:47 +0200

    basic compoent to display a rect according a rect object

---
+++ src/components/SvgMsa/MetadataRect.vue
@@ -0,0 +1,24 @@
+<template>
+  <!-- draw a rectangle according to the value og the object rect -->
+  <rect
+    :x="rect.x"
+    :y="rect.y"
+    :height="rect.height"
+    :width="rect.width"
+    :style="rect.style"
+    :class="rect.class"
+  ></rect>
+</template>
+
+<script>
+export default {
+  props: {
+    rect: {
+      type: Object,
+      default() {
+        return {};
+      }
+    }
+  }
+};
+</script>
