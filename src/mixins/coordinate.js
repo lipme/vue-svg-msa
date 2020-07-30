5336e6f13adc746aad1ce6e032ad802a8a387e09
sallet <erika.sallet@inrae.fr>
2020-07-30 16:35:49 +0200

    add metho to buid rect objects

--- src/mixins/coordinate.js
+++ src/mixins/coordinate.js
@@ -21,6 +21,68 @@ export const coordinate = {
     },
     rectY(i) {
       return this.aY(i) - this.getTrackHeight + 3;
+    },
+    seqWidth(seqLength) {
+      return this.aX(seqLength) - this.aX(0);
+    },
+    /**
+     * Return an array of rectangle objects to display
+     * @param {*} y
+     * @param {*} o
+     * @param {*} o_style
+     * @param {*} seqLen
+     */
+    buildRect(y, o, o_style, seqLen) {
+      let displayedRect = [];
+      if (!o.hasOwnProperty('ranges')) {
+        displayedRect.push(this.newSeqRect(y, seqLen, o_style));
+      } else {
+        o.ranges.forEach(pos => {
+          displayedRect.push(
+            this.newRect(
+              this.rectX(pos[0] - 1),
+              this.rectY(y),
+              (pos[1] - pos[0] + 1) * this.getLetterWidth,
+              this.getTrackHeight,
+              o_style
+            )
+          );
+        });
+      }
+      return displayedRect;
+    },
+    /**
+     * return a new object corresponding to a rect to color all the sequence
+     */
+    newSeqRect(y, seqLen, style, rectclass) {
+      return this.newRect(
+        this.rectX(0),
+        this.rectY(y),
+        this.seqWidth(seqLen),
+        this.getTrackHeight,
+        style,
+        rectclass
+      );
+    },
+
+    newNtRect(x, y, style) {
+      return this.newRect(
+        this.rectX(x),
+        this.rectY(y),
+        this.getLetterWidth,
+        this.getTrackHeight,
+        style
+      );
+    },
+    newRect(x, y, w, h, s, c) {
+      return {
+        x: x,
+        y: y,
+        width: w,
+        height: h,
+        style: s,
+        class: c
+      };
     }
   }
 };
