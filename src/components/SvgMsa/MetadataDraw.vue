e459a3565285c1fc2ab935ca602b5d7907da6f15
sallet <erika.sallet@inrae.fr>
2020-09-29 10:17:10 +0200

    allow to display a metadata object

---
+++ src/components/SvgMsa/MetadataDraw.vue
@@ -0,0 +1,98 @@
+<template>
+  <g>
+    <MetadataRect v-for="(r, index) in coloredRect" :key="'m-' + index" :rect="r"></MetadataRect>
+  </g>
+</template>
+
+<script>
+import { coordinate } from '@/mixins/coordinate';
+import MetadataRect from '@/components/SvgMsa/MetadataRect.vue';
+
+export default {
+  components: {
+    MetadataRect
+  },
+  mixins: [coordinate],
+  props: {
+    metadata: {
+      type: Object,
+      default() {
+        return {};
+      }
+    },
+    seqLen: { type: Number, default: 0 },
+    seqNb: { type: Number, default: 0 },
+    aSeqindex: { type: Function, default: () => {} },
+    selectionSeqs: { type: Array, default: () => [] }
+  },
+  data() {
+    return {
+      defaultOpacity: 0.4,
+      defaultOpacityContrast: 0.2
+    };
+  },
+  computed: {
+    /**
+     * return true if some sequence are selected
+     */
+    selectionMode() {
+      return this.selectionSeqs.length > 0;
+    },
+
+    /**
+     * Get an array of rectangles to display
+     */
+    coloredRect() {
+      let displayedRect = [];
+      // for each categorie
+      this.metadata.categories.forEach(c => {
+        c.regions.forEach(r => {
+          // draw rectangle on the sequence
+          if (r.hasOwnProperty('seqid') && r.seqid !== '') {
+            var regionStyle = this.consolidateStyle(c.style, r.seqid);
+            displayedRect.push(
+              this.buildRect(this.aSeqindex(r.seqid), r, regionStyle, this.seqlen)
+            );
+          } else {
+            // draw rectangle on all the sequences
+            displayedRect.push(this.buildRegionRect(r, this.seqNb, c.style, ''));
+          }
+        });
+      });
+      displayedRect = displayedRect.flat();
+
+      return displayedRect;
+    }
+  },
+  methods: {
+    /**
+     * modify the style according to the sequence: if it is a selected
+     * sequence or not, change the opacity
+     */
+    consolidateStyle(s, seqid) {
+      var regionStyle = Object.assign({}, s);
+
+      if (
+        regionStyle.hasOwnProperty('fill') &&
+        !regionStyle.hasOwnProperty('fill-opacity') &&
+        seqid !== ''
+      ) {
+        // fill the opacity according to the state of the sequence (selected or not)
+        if (
+          this.selectionMode === false ||
+          (this.selectionMode === true && this.selectionSeqs.find(o => o.id == seqid) !== undefined)
+        ) {
+          regionStyle['fill-opacity'] = this.defaultOpacity;
+        } else {
+          regionStyle['fill-opacity'] = this.defaultOpacity - this.defaultOpacityContrast;
+        }
+      }
+      if (!regionStyle.hasOwnProperty('fill-opacity')) regionStyle['fill-opacity'] = 0.0;
+
+      return regionStyle;
+    }
+  }
+};
+</script>
+
+<style></style>
