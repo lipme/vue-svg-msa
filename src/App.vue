493230e24f6c0a40d98d7ffaf339cd0f901e1708
sallet <erika.sallet@inrae.fr>
2020-10-15 17:14:37 +0200

    add a complete test in App

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,44 @@
 <template>
   <div id="app">
-    <svg-msa
-      :start="start"
-      :end="end"
-      :seqs="testSeqsNEW"
-      :tracks="testTracks"
-      coloring="metadata"
-      type="nt"
-      :metadata="metadata"
-      :selectedseqs="selectedseqid"
-    ></svg-msa>
+    <h2>Test vue-svg-msa component</h2>
+    <button @click="loadExample">Load an exemple (11 sequences)</button>
+    <hr width="100%" />
+    <h4>Parameters</h4>
+    <label>Start (1-based) </label
+    ><input v-model.number="start" type="number" placeholder="start position" /> <label>End</label
+    ><input v-model.number="end" type="number" placeholder="stop position" />
+    <label>Selected regions</label><input v-model="selectedregions" placeholder="4-4,70-80" />
+    <label>Selected sequences</label>
+    <select id="select1" v-model="selectedseqs" multiple>
+      <option v-for="seq in testSeqs" :key="seq.id" :value="seq.id">{{ seq.name }}</option>
+    </select>
+    <p><button @click="applyParam">Apply</button><button @click="reinit">Reinit</button></p>
 
-    <!-- <svg-msa :seqs="seqs2" coloring="auto"></svg-msa> -->
+    <hr width="100%" />
+    <p>
+      <label>Coloring</label>
+      <select v-model="coloring">
+        <option>no</option>
+        <option>seqcolor</option>
+        <option>auto</option>
+        <option>metadata</option>
+      </select>
+    </p>
+
+    <hr width="100%" />
+
+    <h3>Rendering</h3>
+    <svg-msa
+      v-if="seqs.length > 0"
+      :start="activestart"
+      :end="activeend"
+      :seqs="seqs"
+      :tracks="tracks"
+      :coloring="activecoloring"
+      :selectedseqs="selectedseqs"
+      :metadata="activemetadata"
+      type="nt"
+    ></svg-msa>
   </div>
 </template>
 
@@ -25,157 +52,141 @@ export default {
   },
   data() {
     return {
-      start: 5,
-      end: -1,
-      selectedseqid: ['seq1', 'seq2', 'seq5'],
-      // selectedseqid: [],
+      selectedseqs: [],
+      activestart: 1,
+      activeend: 0,
+      activeselectedfield: '',
+      activeselectedregions: '',
+      start: 1,
+      end: 0,
+      seqs: [],
+      selectedfield: '',
+      selectedregions: '',
 
-      metadata: [
-        {
-          label: 'Conservation level',
-          categories: [
-            {
-              label: 'high level',
-              style: {
-                fill: 'red'
-              },
-              regions: [
-                {
-                  id: 'seq1',
-                  ranges: [[21, 176]]
-                },
-                {
-                  id: 'seq2',
-                  ranges: [[21, 176]]
-                },
-                {
-                  id: 'seq3',
-                  ranges: [[21, 176]]
-                },
-                {
-                  id: 'seq4',
-                  ranges: [[21, 176]]
-                },
-                {
-                  id: 'seq5',
-                  ranges: [[21, 176]]
-                },
-                {
-                  id: 'seq6',
-                  ranges: [[21, 176]]
-                },
-                {
-                  id: 'seq7',
-                  ranges: [[21, 176]]
-                },
-                {
-                  id: 'seq8',
-                  ranges: [[21, 176]]
-                },
-                {
-                  id: 'seq9',
-                  ranges: [[21, 176]]
-                },
-                {
-                  id: 'seq10',
-                  ranges: [[21, 176]]
-                },
-                {
-                  id: 'seq11',
-                  ranges: [[21, 176]]
-                }
-              ]
+      metadata: {
+        label: 'Conservation level',
+        categories: [
+          {
+            label: 'high level',
+            style: {
+              fill: 'red'
             },
-            {
-              label: 'moderate level',
-              style: {
-                fill: 'blue'
+            regions: [
+              {
+                id: 'seq1',
+                ranges: [[21, 176]]
               },
-              regions: [
-                {
-                  id: 'seq1',
-                  ranges: [
-                    [1, 20],
-                    [177, 234]
-                  ]
-                },
-                {
-                  id: 'seq2',
-                  ranges: [
-                    [1, 20],
-                    [177, 234]
-                  ]
-                },
-                {
-                  id: 'seq3',
-                  ranges: [
-                    [1, 20],
-                    [177, 234]
-                  ]
-                },
-                {
-                  id: 'seq4',
-                  ranges: [
-                    [1, 20],
-                    [177, 234]
-                  ]
-                },
-                {
-                  id: 'seq5',
-                  ranges: [
-                    [1, 20],
-                    [177, 234]
-                  ]
-                },
-                {
-                  id: 'seq6',
-                  ranges: [[177, 234]]
-                },
-                {
-                  id: 'seq7',
-                  ranges: [[177, 234]]
-                },
-                {
-                  id: 'seq8',
-                  ranges: [[177, 234]]
-                },
-                {
-                  id: 'seq9',
-                  ranges: [[177, 234]]
-                },
-                {
-                  id: 'seq10',
-                  ranges: [[177, 234]]
-                },
-                {
-                  id: 'seq11',
-                  ranges: [[177, 234]]
-                }
-              ]
-            }
-          ]
-        },
-        {
-          label: 'polymorph region',
-          categories: [
-            {
-              label: 'polymorph region',
-              style: {
-                'fill-opacity': 0.3,
-                fill: 'black'
+              {
+                id: 'seq2',
+                ranges: [[21, 176]]
               },
-              regions: [
-                {
-                  ranges: [
-                    [35, 35],
-                    [90, 96]
-                  ]
-                }
-              ]
-            }
-          ]
-        }
-      ],
+              {
+                id: 'seq3',
+                ranges: [[21, 176]]
+              },
+              {
+                id: 'seq4',
+                ranges: [[21, 176]]
+              },
+              {
+                id: 'seq5',
+                ranges: [[21, 176]]
+              },
+              {
+                id: 'seq6',
+                ranges: [[21, 176]]
+              },
+              {
+                id: 'seq7',
+                ranges: [[21, 176]]
+              },
+              {
+                id: 'seq8',
+                ranges: [[21, 176]]
+              },
+              {
+                id: 'seq9',
+                ranges: [[21, 176]]
+              },
+              {
+                id: 'seq10',
+                ranges: [[21, 176]]
+              },
+              {
+                id: 'seq11',
+                ranges: [[21, 176]]
+              }
+            ]
+          },
+          {
+            label: 'moderate level',
+            style: {
+              fill: 'blue'
+            },
+            regions: [
+              {
+                id: 'seq1',
+                ranges: [
+                  [1, 20],
+                  [177, 234]
+                ]
+              },
+              {
+                id: 'seq2',
+                ranges: [
+                  [1, 20],
+                  [177, 234]
+                ]
+              },
+              {
+                id: 'seq3',
+                ranges: [
+                  [1, 20],
+                  [177, 234]
+                ]
+              },
+              {
+                id: 'seq4',
+                ranges: [
+                  [1, 20],
+                  [177, 234]
+                ]
+              },
+              {
+                id: 'seq5',
+                ranges: [
+                  [1, 20],
+                  [177, 234]
+                ]
+              },
+              {
+                id: 'seq6',
+                ranges: [[177, 234]]
+              },
+              {
+                id: 'seq7',
+                ranges: [[177, 234]]
+              },
+              {
+                id: 'seq8',
+                ranges: [[177, 234]]
+              },
+              {
+                id: 'seq9',
+                ranges: [[177, 234]]
+              },
+              {
+                id: 'seq10',
+                ranges: [[177, 234]]
+              },
+              {
+                id: 'seq11',
+                ranges: [[177, 234]]
+              }
+            ]
+          }
+        ]
+      },
       mmm: [
         {
           label: 'drought resistance',
@@ -206,6 +217,85 @@ export default {
               regions: []
             }
           ]
+        }
+      ],
+      testSeqs: [
+        {
+          seq:
+            'ATCATCATCGGGGGATACTCATTTTTACATCATCATCATACTACATGACGACGACAGCAGCCCTTACGACGATTTACGCGAGAACACTATCATCACAGACGAATTAGAGAGGGGGACAGAGGGACAGCAGCAGCCCAGACAGACGACCCGATTTATACTCATACTCACAGCATACGATGACACAGACGACGACAGCAGACGACGCATCATATACTCATTTTTACATCATCAAT',
+          id: 'seq1',
+          name: 'seq1',
+          color: 'red'
+        },
+        {
+          seq:
+            'ATCATCATCATCATCATACTCATTTTTACATCATCATCATACTACATGACGACGACAGCAGCCCTTACGACGATTTACGCGAGAACACTATCATCACAGACGAATTAGAGAGGGGGACAGAGGGACAGCAGCAGCCCAGACAGACGACCCGATTTATACTCATACTCACAGCATAC----ACACAGACGACGACAGCAGTCGACGTTTCATATACTCA---TTACATCATCAAT',
+          id: 'seq2',
+          name: 'seq2',
+          color: 'red'
+        },
+        {
+          seq:
+            'ATCATCATCATCATCATACTCATTTTTACATCATCATCATACTACATGACGACGACAGCAGCCC-GACG--GATTTACGCGAGAACACTATCATCACAGACGAATTAGAGAGGGGGACAGAGGGACAGCAGCAGCCCAGACAGACGACCCGATTTATACTCATACTCACAGCATACGATGACACAGACGACGACAGCAGACGACGCATCATATACTCATTTTTACATCATCAAT',
+          id: 'seq3',
+          name: 'seq3',
+          color: 'red'
+        },
+        {
+          seq:
+            'ATCATCATCATCATGGTACTCA---TTACATCATCATCATACTACATGACGACGACAGCAGCCCTTACGACGATTTACGCGAGAACACTATCATCACAGACGAATT------------AGAGGGACAGCAGCAGCCCAGACAGACGACCCGATTTATACTCATACTCACAGCATACGATGACACAGACGACGACAGCAGACGACGCATCATATACTCATTTTTACATCATCAAT',
+          id: 'seq4',
+          name: 'seq4',
+          color: 'blue'
+        },
+        {
+          seq:
+            'ATCATCATCATCATGGTACTCATTTTTACATCATCATCATACTACATGACGACGACAGCAGCCCTTACGACGATTTACGCGAGAACACTATCATCACAGACGAATT------------AGAGGGACAGCAGCAGCCCAGACAGACGACCCGATTTATACTCATACTCACAGCATACGATGACACAGACGACGACAGCAGACGACGCATCATATACTCATTTTTACATCATCAAT',
+          id: 'seq5',
+          name: 'seq5',
+          color: 'blue'
+        },
+        {
+          seq:
+            '--------------------CATTTTTACATCATCATCATACTACATGACGACGACAGCAGCCCTTACGACGATTTACGCGAGAACACTATCATCACAGACGAATTAGAGAGGGGGACAGAGGGACAGCAGCAGCCCAGACAGACGACCCGATTTATACTCATACTCACAGCATACGATGACACAGACGACGACAGCAGACGACGCATCATATACTCATTTTTACATCATCAAT',
+          id: 'seq6',
+          name: 'seq6',
+          color: 'green'
+        },
+        {
+          seq:
+            '--------------------CATTTTTACATCATCATCATACTACATGACGACGACAGCAGCCCTTACGACGATTTACGCGAGAACACTATCATCACAGACGAATTAGAGAGGGGGACAGAGGGACAGCAGCAGCCCAGACAGACGACCCGATTTATACTCATACTCACAGCATACGATGACACAGACGACGACAGCAGACGACGCATCATATACTCATTTTTACATCATCAAT',
+          id: 'seq7',
+          name: 'seq7',
+          color: 'green'
+        },
+        {
+          seq:
+            '--------------------CATTTTTACATCATCATCATACTACATGACGACGACAGCAGCCCTTACGACGATTTACGCGAGAACACTATCATCACAGACGAA----------GGACAGAGGGACAGCAGCAGCCCAGACAGACGACCCGATTTATACTCATACCCACAGCATACGATGACACAGACGACGACAGCAGACGACGCATCATATACTCATTTTTACATCAGGAAT',
+          name: 'seq8',
+          id: 'seq8',
+          color: 'green'
+        },
+        {
+          seq:
+            '--------------------CATTTTTACATCATCATCATACTACATGACGACGACAGCAGCCCTTACGACGATTTACGCGAGAACACTATCATCACAGACGAATTAGA-----GGACAGAGGGACAGCAGCAGCCCAGACAGACGACCCGATTTATACTCATACTCACAGCATACGATGACACAGACGACGACAGCAGACGACGCATCATATACTCATTTTTACATCATCAAT',
+          id: 'seq9',
+          name: 'seq9',
+          color: 'green'
+        },
+        {
+          seq:
+            '--------------------CATTTTTACATCATCATCATACTACATGACGACGACAGCAGCCCTTACGACGATTTACGCGAGAACACTATCATCACAGACGAATTAGAGA---TGACAGAGGGACAGCAGCAGCCCAGACAGACGACCCGATTTATACTCATACTCACAGCATACGATGACACAGACGACGACAGCAGACGACGCATCATATACTCATTTTTACATCATCAAT',
+          id: 'seq10',
+          name: 'seq10',
+          color: 'green'
+        },
+        {
+          seq:
+            '--------------------CATTTTTACATCATTATCATACTACATGACGACGACAGCAGCCCTTACGACGATTTACGCGAGAACACTATCATCACAGACGAATTAGAGA---T-TCAGAGGGACAGCAGCAGCCCAGACAGACGACCCGATTTATACTCATACTCACAGCATACGATGACACAGACGACGACAGCAGACGACGCATCATATACTCATTTTTACATCATCAAT',
+          id: 'seq11',
+          name: 'seq11',
+          color: 'green'
         }
       ],
       testSeqsNEW: [
@@ -325,8 +415,84 @@ export default {
           trackLabel: 'track2'
         }
       ],
-      coloring: 'auto' //'auto'
+      coloring: 'metadata' //'auto'
     };
+  },
+  computed: {
+    getselectedids() {
+      return this.activeselectedseqs;
+    },
+    selectedregionsArray() {
+      return [
+        {
+          ranges: this.selectedregions.split(',').map(a => a.split('-'))
+        }
+      ];
+    },
+    getselectedregioddns() {
+      return [];
+    },
+    activecoloring() {
+      //let c = this.coloring;
+      return this.coloring + '|metadata';
+    },
+    activemetadata() {
+      let m = [];
+      if (this.coloring === 'metadata') {
+        m.push(this.metadata);
+      }
+      if (this.activeselectedregions != '') {
+        m.push(this.getSelectedRegions());
+      }
+      return m;
+    }
+  },
+  methods: {
+    loadExample() {
+      this.seqs = this.testSeqs;
+      this.tracks = this.testTracks;
+      this.reinit();
+    },
+    applyParam() {
+      (this.activestart = this.start),
+        (this.activeend = this.end),
+        (this.activeselectedfield = this.selectedfield),
+        (this.activeselectedseqs = []),
+        (this.activeselectedseqs = this.selectedseqs),
+        (this.activeselectedregions = this.selectedregions);
+    },
+    initParam() {
+      (this.start = 1),
+        (this.end = 0),
+        (this.selectedfield = ''),
+        (this.selectedregions = ''),
+        (this.selectedseqs = []),
+        (this.activeselectedseqs = []);
+    },
+    reinit() {
+      this.initParam();
+      this.applyParam();
+    },
+
+    getSelectedRegions() {
+      let r = this.selectedregionsArray;
+
+      let m = {
+        label: 'select',
+        categories: [
+          {
+            label: '',
+            style: {
+              'fill-opacity': 0.3,
+              fill: 'black'
+            },
+            regions: r
+          }
+        ]
+      };
+      console.log(m);
+      return m;
+    }
   }
 };
 </script>
